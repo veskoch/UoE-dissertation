8f73de16579b503db0119f016f3f9195c55fba77
Vesko <cholakovveselin@gmail.com>
2018-06-26 21:55:03 +0100

    1.1_load_data can now collate, convert to NoteSeq and serialize to .tfrecord

--- .ipynb_checkpoints/constants-checkpoint.py
+++ .ipynb_checkpoints/constants-checkpoint.py
@@ -1,9 +1,30 @@
+import magenta
+from magenta.models.performance_rnn import performance_model
+import tensorflow as tf
+
 """Constants for music processing."""
 
-SRC_FOLDER = "/Users/vesko/Google Drive/Docs/Education/Edinburgh/Classes/DISS/Data/MSc 2018 Research/Preprocessed Dataset"
+SOURCE_DIR = "/Users/vesko/Google Drive/Docs/Education/Edinburgh/Classes/DISS/Data/MSc 2018 Research/Preprocessed Dataset"
+
+COLLATED_DIR = "./tmp/collated/"
 
 INPUT_DIR = "./tmp/raw_xml/"
 TFRECORD_FILE = "./tmp/test.tfrecord"
 OUTPUT_DIR = "./tmp/sequence_examples/"
 EVAL_RATIO = 0.2
 CONFIG = 'performance'
+
+default_configs = {
+    'performance': performance_model.PerformanceRnnConfig(
+        magenta.protobuf.generator_pb2.GeneratorDetails(
+            id='performance',
+            description='Performance RNN'),
+        magenta.music.OneHotEventSequenceEncoderDecoder(
+            magenta.music.PerformanceOneHotEncoding()),
+        tf.contrib.training.HParams(
+            batch_size=64,
+            rnn_layer_sizes=[512, 512, 512],
+            dropout_keep_prob=1.0,
+            clip_norm=3,
+            learning_rate=0.001))
+}
