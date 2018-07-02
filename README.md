3109e309385ae3318001ce2daf430e8beb56834b
Vesko <cholakovveselin@gmail.com>
2018-07-02 23:36:04 +0100

    organize + write documentation

--- README.md
+++ README.md
@@ -7,6 +7,13 @@ This repository contains the code for the author's dissertation at the MSc Artif
 The proposed dissertation project is building on the recent momentum in applying deep learning to music-related tasks. Most of the academic and private sector efforts have gone to devise full algorithmic composition of new content imitating the training corpus. However, the present research takes a different approach. The author looks at building a technology which can support rather than supplant the human creator. Over 12 weeks in the summer, the author will have access to a never-before-used dataset from the music industry to design a Recurrent Neural Network- based architecture which can transform simple monotonic melodic cues from popular songs to elaborate compositions backed by chords, taking extreme ranges and varying rhythmically and dynamically – a reconceptualization known in music jargon as arrangement.
 
 
+## Workflow ##
+
+On a high level, the process is:
+
+Collate > Preprocess > Build Vocab > Define Model > Train & Eval
+
+Each of those steps comes with a set of possible configurations which are explored for the dissertation.
 
 ## Dependencies ##
 The instruction below pertain to Mac OSX High Sierra.
@@ -15,14 +22,13 @@ The instruction below pertain to Mac OSX High Sierra.
 ### System-level ###
 * Python >= 3.6
 * Anaconda
-* [FluidSynth](http://www.fluidsynth.org/) = 1.1.11
+* [fluidsynth](http://www.fluidsynth.org/) = 1.1.11
     * `brew install fluidsynth pkg-config`
+    * FluidSynth is a software synthesizer for generating music. It's the software analogue of a MIDI synthesizer.  You load patches, set parameters, then send NOTEON and NOTEOFF events to play notes. Instruments are defined in SoundFonts, generally files with the extension SF2. FluidSynth can either be used to play audio itself, or you can call a function that returns chunks of audio data and output the data to the soundcard yourself.
+    * This is not a `pip` install! There is a module in PyPI of the same name but it is not what we need. It's very confusing.
 * [gsutil](https://cloud.google.com/storage/docs/gsutil_install)
     * You need `gsutil` to download the Alexander Holm Salamander piano SoundFont from a Magenta GCS bucket (591.9 MiB)
         * `gsutil -m cp gs://download.magenta.tensorflow.org/soundfonts/Yamaha-C5-Salamander-JNv5.1.sf2 /tmp/`
-* fluidsynth
-    * The software analogue of a MIDI synthesizer.
-    * This is not a `pip` install! There is a module in PyPI of the same name but it is not what we need.
 
 ### Python Libraries ###
 
@@ -36,7 +42,7 @@ You can install most (if not all) with `pip install`.
 * scipy >= 0.18.1
 * pretty_midi >= 0.2.6
 * [pyfluidsynth](https://github.com/nwhitehead/pyfluidsynth)
-    * This module contains python bindings for FluidSynth. FluidSynth is a software synthesizer for generating music. It works like a MIDI synthesizer. You load patches, set parameters, then send NOTEON and NOTEOFF events to play notes. Instruments are defined in SoundFonts, generally files with the extension SF2. FluidSynth can either be used to play audio itself, or you can call a function that returns chunks of audio data and output the data to the soundcard yourself.
+    * This module contains python bindings for FluidSynth.
 * bokeh >= 0.12.0
 
 
@@ -74,9 +80,6 @@ For more information/enquiries contact one the administrators of the repository.
 
 #### Author ####
 * Vesko Cholakov
-
-
-
 
 
 
---
+++ process_data.py
@@ -0,0 +1,178 @@
+import tensorflow as tf
+
+import magenta
+
+from magenta.music import performance_lib
+from magenta.pipelines import pipeline
+from magenta.pipelines import statistics
+from magenta.protobuf import music_pb2
+from magenta.pipelines.pipeline import _guarantee_dict
+
+import os
+
+class PerformanceExtractor(pipeline.Pipeline):
+    """Extracts polyphonic tracks from a quantized NoteSequence."""
+
+    def __init__(self, min_events, max_events, num_velocity_bins, name=None):
+        super(PerformanceExtractor, self).__init__(
+            input_type=music_pb2.NoteSequence,
+            output_type=magenta.music.Performance,
+            name=name)
+        self._min_events = min_events
+        self._max_events = max_events
+        self._num_velocity_bins = num_velocity_bins
+
+    def transform(self, quantized_sequence):
+        performances, stats = magenta.music.extract_performances(
+            quantized_sequence,
+            num_velocity_bins=self._num_velocity_bins)
+        self._set_stats(stats)
+        return performances
+    
+class PerformanceParser(pipeline.Pipeline):
+    """Converts a Performance into a text sequence.
+    
+    Individual events become 'words' of A-Z 0-9 separated by space. 
+    """
+    
+    def __init__(self, name=None):
+        super(PerformanceParser, self).__init__(
+            input_type=magenta.music.Performance,
+            output_type=str,
+            name=name)
+        
+    def transform(self, performance):
+        strs = []
+        for event in performance:
+            if event.event_type == performance_lib.PerformanceEvent.NOTE_ON:
+                strs.append('ON%s' % event.event_value)
+            elif event.event_type == performance_lib.PerformanceEvent.NOTE_OFF:
+                strs.append('OFF%s' % event.event_value)
+            elif event.event_type == performance_lib.PerformanceEvent.TIME_SHIFT:
+                strs.append('SHIFT%s' % event.event_value)
+            else:
+                raise ValueError('Unknown event type: %s' % event.event_type)
+        return [' '.join(strs)]
+
+def run_pipeline_text(pipeline,
+                      input_iterator,
+                      output_dir):
+    """Runs a pipeline graph saving output to disk as text.
+     
+    Run the the pipeline on each input from the iterator one at a time.
+    A file will be written to `output_dir` for each dataset name specified
+    by the pipeline. pipeline.transform is called on each input and the
+    results are aggregated into their correct datasets.
+
+    The output type given by `pipeline.output_type` must be str.
+
+    Args:
+        pipeline: A Pipeline instance. `pipeline.output_type` must be a str.
+        input_iterator: Iterates over the input data. Items returned by it are fed
+            directly into the pipeline's `transform` method.
+        output_dir: Path to directory where datasets will be written. Each dataset
+            is a file whose name contains the pipeline's dataset name. If the
+            directory does not exist, it will be created.
+            
+    Raises:
+        ValueError: If any of `pipeline`'s output type is not str.
+     
+    """
+    
+    if isinstance(pipeline.output_type, dict):
+        for name, type_ in pipeline.output_type.items():
+            if type_ != str:
+                raise ValueError(
+                    'Pipeline "%s" must output %s type. '
+                    'Output type was %s' % (name, str, type_))
+    else:
+         if type_ != str:
+            raise ValueError(
+                    'Pipeline "%s" must output %s type. '
+                            'Output type was %s' % (name, str, pipeline.output_type))
+    
+    
+    aggregated_outputs = dict(
+        [(name, []) for name in pipeline.output_type_as_dict])
+    total_inputs = 0
+    total_outputs = 0
+    stats = []
+    
+    output_names = pipeline.output_type_as_dict.keys()
+    output_paths = [os.path.join(output_dir, name + '.txt')
+                    for name in output_names]
+
+    for path in output_paths:
+        if os.path.exists(path):
+            raise FileExistsError('File {} already exists. Please remove and try again.'
+                                        .format(path))           
+
+    writers = dict([(name, open(path, 'a'))
+                  for name, path in zip(output_names, output_paths)])
+    
+    for input_object in input_iterator:
+        total_inputs += 1
+        
+        for name, outputs in _guarantee_dict(
+            pipeline.transform(input_object),
+            list(output_names)[0]).items():
+            
+            for output in outputs:
+                writers[name].write(output + '\n')
+                
+            total_outputs += len(outputs)
+        stats = statistics.merge_statistics(stats + pipeline.get_stats())
+        if total_inputs % 5000 == 0:
+            tf.logging.info('Processed %d inputs so far. Produced %d outputs.', 
+                            total_inputs, total_outputs)
+            statistics.log_statistics_list(stats, tf.logging.info)
+    tf.logging.info('\n\nCompleted.\n')
+    tf.logging.info('Processed %d inputs total. Produced %d outputs.',
+                    total_inputs, total_outputs)
+    statistics.log_statistics_list(stats, tf.logging.info)
+    return aggregated_outputs
+
+def build_dataset(pipeline_config, pipeline_graph_def):
+    for collection_name in ['inputs', 'targets']:
+        
+        src_file = os.path.join(pipeline_config['data_source_dir'], 
+                                   collection_name + '.tfrecord')
+        output_dir = pipeline_config['data_target_dir']
+        
+        # Construct the pipeline graph
+        pipeline_graph = pipeline_graph_def(
+            collection_name = collection_name,
+            config = pipeline_config
+        )
+
+        # Runs pipeline graph on a data source and writes output to dir
+        run_pipeline_text(
+            pipeline_graph,
+            pipeline.tf_record_iterator(src_file,
+                                        pipeline_graph.input_type),
+            output_dir
+        )
+
+def build_vocab(pipeline_config):
+    """ This method stands on its own. Invoke after everything else.
+    You need to change this method if you change the Encoding Format,
+    or the Encoding Representation.
+    """
+    
+    file_path = pipeline_config['data_target_dir'] + 'vocab.txt'
+    
+    vocab = {
+        'ON': (0, 127 + 1),
+        'OFF': (0, 127 + 1),
+        'SHIFT': (0, pipeline_config['steps_per_second'] + 1),
+    }  
+    
+    if os.path.exists(file_path):
+        print("INFO: File {} exists. Removing. Rebuilding vocabulary.".format(file_path))
+        os.remove(file_path)
+    with open(file_path, 'a') as file:
+        for action in vocab.keys():
+            for val in range(vocab[action][0], vocab[action][1]):
+                file.write(action + str(val) + '\n')
+                
+    print("INFO: Vocabulary built.")
