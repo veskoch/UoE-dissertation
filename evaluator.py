1a63154e5829e8d6e54810f7145cf4ecda1caf30
Vesko <cholakovveselin@gmail.com>
2018-07-09 19:46:15 +0100

    lots of new external musical scorers

--- evaluator.py
+++ evaluator.py
@@ -2,6 +2,7 @@
 
 from statistics import mean
 import subprocess
+import tempfile
 
 import abc
 import io
@@ -10,9 +11,12 @@ import os
 import six
 
 import tensorflow as tf
+import numpy as np
 
 from opennmt.utils.misc import get_third_party_dir
 from utils.text_seqs import TextSequence, TextSequenceCollection
+
+from collections import Counter
 
 import music21
 
@@ -155,7 +159,7 @@ class BLEUDetokEvaluator(BLEUEvaluator):
     return "BLEU-detok"
 
 
-class TonalConfidence(ExternalEvaluator):
+class MusicScores(ExternalEvaluator):
     """Calculates the 1) Key Correlation Coefficient and 2) Key Tonal Certainty.
     Reports both 1) in absolute terms for predictions, and as relative between
     labels and predictions. 
@@ -164,10 +168,6 @@ class TonalConfidence(ExternalEvaluator):
         piece in that key.
     Key Tonal Certainty: Provides a measure of tonal ambiguity for Key determined 
     with one of many methods.
-
-    In Absolute terms the best possible score is 1, and 0 is the worst.
-    In Relative terms, the best possible score is 0. Range is [-1,1], though
-        negative values are unlikely.
     
     """
 
@@ -179,45 +179,148 @@ class TonalConfidence(ExternalEvaluator):
         labels = TextSequenceCollection(labels_file)
         predictions = TextSequenceCollection(predictions_path)
 
-        score = {
-            cc : { 'labels' : [], 'predictions' : [] },
-            tc : { 'labels' : [], 'predictions' : [] }
+        datapoints = {
+            'cc' : { 'labels' : [], 'predictions' : [] }, # Correlation Coefficient
+            'tc' : { 'labels' : [], 'predictions' : [] }, # Tonal Certainty
+            'key_name_matches' : 0.0,
+            'key_mode_matches' : 0.0,
+            'tempo' : { 'labels' : [], 'predictions' : [] }, # in bmp
+            'duration' : { 'labels' : [], 'predictions' : [] } # in seconds
+        }
+
+        results = {
+            'cc' : 0.0,
+            'tc' : 0.0,
+            'cc_dist' : 0.0,
+            'tc_dist' : 0.0,
+            'key_name_acc' : 0.0,
+            'key_mode_acc' : 0.0,
+            'tempo' : 0.0,
+            'tempo_dist' : 0.0,
+            'duration_dist' : 0.0
         }
         
-        for midi in labels.as_midi:
-            score = music21.converter.parse(midi) # <music21.stream.Score ...> dtype
-            key = score.analyze('key')
-            score['cc']['labels'].append(key.correlationCoefficient)
-            score['tc']['labels'].append(key.tonalCertainty())
+        assert len(labels) == len(predictions)
 
-        for midi in predictions.as_midi:
-            score = music21.converter.parse(midi) # <music21.stream.Score ...> dtype
-            key = score.analyze('key')
-            score['cc']['predictions'].append(key.correlationCoefficient)
-            score['tc']['predictions'].append(key.tonalCertainty())
-        
-        score['cc']['labels'] = mean(score['cc']['labels'])
-        score['tc']['labels'] = mean(score['tc']['labels'])
-        score['cc']['predictions'] = mean(score['cc']['predictions'])
-        score['tc']['predictions'] = mean(score['tc']['predictions'])
-    
+        # Iterate in parallel over all (label, prediction) pairs
+        for i in range(len(labels)):
+            
+            label_midi = labels.as_midi[i]
+            prediction_midi = predictions.as_midi[i]
+            
+            with tempfile.NamedTemporaryFile(suffix='.midi') as temp_file:
+                # call write on pretty_midi, and write to a temporary file
+                label_midi.write(temp_file.name)
+                mf = music21.midi.MidiFile()
+                mf.open(temp_file.name)
+                mf.read()
+                mf.close()
+                label_stream = music21.midi.translate.midiFileToStream(mf)
+
+             with tempfile.NamedTemporaryFile(suffix='.midi') as temp_file:
+                # call write on pretty_midi, and write to a temporary file
+                prediction_midi.write(temp_file.name)
+                mf = music21.midi.MidiFile()
+                mf.open(temp_file.name)
+                mf.read()
+                mf.close()
+                prediction_stream = music21.midi.translate.midiFileToStream(mf)           
+            
+            label_key = label_stream.analyze('key')
+            prediction_key = prediction_stream.analyze('key')
+            
+            ###########################  
+            ### COLLECT DATA-POINTS ###
+
+            # Correlation Coefficient
+            datapoints['cc']['labels'].append(label_key.correlationCoefficient)
+            datapoints['cc']['predictions'].append(prediction_key.correlationCoefficient)
+
+            # Tonal Certainty
+            datapoints['tc']['labels'].append(label_key.tonalCertainty())
+            datapoints['tc']['predictions'].append(prediction_key.tonalCertainty())
+
+            # Is prediction in same key as labels?
+            if label_key.tonic.name == prediction_key.tonic.name:
+                datapoints['key_name_matches'] += 1
+
+            # Is prediction same mode as labels?
+            if label_key.mode == prediction_key.mode:
+                datapoints['key_mode_matches'] += 1
+
+            # Tempo (in bmp)
+            try: # a few (not many) files fail, usually those with one note bar
+                datapoints['tempo']['labels'].append(label_midi.estimate_tempo())
+                datapoints['tempo']['predictions'].append(prediction_midi.estimate_tempo())
+            except:
+                pass
+
+            # Duration
+            datapoints['tempo']['labels'].append(label_midi.get_end_time())
+            datapoints['tempo']['predictions'].append(prediction_midi.get_end_time())
+
+        #############################  
+        ### SUMMARIZE DATA-POINTS ###
+
+        # Correlation Coefficient
+        datapoints['cc']['labels'] = np.array(datapoints['cc']['labels'])
+        datapoints['cc']['predictions'] = np.array(datapoints['cc']['predictions'])
+        results['cc'] = np.mean(datapoints['cc']['predictions'])
+        results['cc_dist'] = np.mean(np.abs(np.array(datapoints['cc']['labels']) - np.array(datapoints['cc']['predictions'])))
+
+        # Tonal Certainty
+        datapoints['tc']['labels'] = np.array(datapoints['tc']['labels'])
+        datapoints['tc']['predictions'] = np.array(datapoints['tc']['predictions'])
+        results['tc'] = np.mean(datapoints['tc']['predictions'])
+        results['tc_dist'] = np.mean(np.abs(np.array(datapoints['tc']['labels']) - np.array(datapoints['tc']['predictions'])))
+
+        # Key Accuracy
+        results['key_name_acc'] = datapoints['key_name_acc'] / float(len(labels))
+        results['key_mode_acc'] = datapoints['key_mode_acc'] / float(len(labels))
+
+        # Tempo
+        datapoints['tempo']['predictions'] = np.array(datapoints['tempo']['predictions'])
+        datapoints['tempo']['labels'] = np.array(datapoints['tempo']['labels'])
+        results['tempo'] = np.mean(datapoints['tempo']['predictions'])
+        results['tempo_dist'] = np.mean(np.abs(datapoints['tempo']['labels'] - datapoints['tempo']['predictions']))
+
+        # Duration
+        results['duration_dist'] = np.mean(np.abs(datapoints['duration']['labels'] - datapoints['duration']['predictions']))
+
+        return results
+
     def _summarize_score(self, step, score):
-        self._summarize_value(step, "Tonal/Correlation_Coefficient", score["cc"]['predictions'])
-        self._summarize_value(step, "Tonal/Tonal_Certainty", score["tc"]['predictions'])
+        self._summarize_value(step, "Correlation_Coefficient/Score", score['cc'])
+        self._summarize_value(step, "Correlation_Coefficient/Distance", score['cc_dist'])
 
-        self._summarize_value(step, "Tonal_Relative/Correlation_Coefficient", score["cc"]['labels'] - score["cc"]['predictions'])
-        self._summarize_value(step, "Tonal_Relative/Tonal_Certainty", score["tc"]['labels'] - score["tc"]['predictions'])
+        self._summarize_value(step, "Tonal_Certainty/Score", score['tc'])
+        self._summarize_value(step, "Tonal_Certainty/Distance", score['tc_dist'])
+
+        self._summarize_value(step, "Key_Accuracy/Key_Name", score['key_name_acc'])
+        self._summarize_value(step, "Key_Accuracy/Key_Mode", score['key_mode_acc'])
+
+        self._summarize_value(step, "Tempo/Real_(bpm)", score['tempo'])
+        self._summarize_value(step, "Tempo/Distance_(s)", score['tempo_dist'])
+
+        self._summarize_value(step, "Duration/Distance_(s)", score['duration_dist'])
 
     def _log_score(self, score):
-        tf.logging.info("Key Correlation Coefficient (on predictions):\t{}".format(score["cc"]['predictions']))
-        tf.logging.info("Key Tonal Certainty (on predictions):\t{}".format(score["tc"]['predictions']))
+        tf.logging.info("Correlation_Coefficient/Score \t {}".format(score['cc']))
+        tf.logging.info("Correlation_Coefficient/Distance \t {}".format(score['cc_dist']))
 
-        tf.logging.info("Key Correlation Coefficient (predictions relative to labels):\t{}"
-            .format(score["cc"]['labels'] - score["cc"]['predictions']))
-        tf.logging.info("Key Tonal Certainty (predictions relative to labels):\t{}"
-            .format(score["tc"]['labels'] - score["tc"]['predictions']))
+        tf.logging.info("Tonal_Certainty/Score \t {}".format(score['tc']))
+        tf.logging.info("Tonal_Certainty/Distance \t {}".format(score['tc_dist']))
 
+        tf.logging.info("Key_Accuracy/Key_Name \t {}".format(score['key_name_acc']))
+        tf.logging.info("Key_Accuracy/Key_Mode \t {}".format(score['key_mode_acc']))
 
+        tf.logging.info("Tempo/Real_(bpm) \t {}".format(score['tempo']))
+        tf.logging.info("Tempo/Distance_(s) \t {}".format(score['tempo_dist']))
+
+        tf.logging.info("Duration/Distance_(s) \t {}".format(score['duration_dist']))
+        
+
+        
 def external_evaluation_fn(evaluators_name, labels_file, output_dir=None):
   """Returns a callable to be used in
   :class:`opennmt.utils.hooks.SaveEvaluationPredictionHook` that calls one or
